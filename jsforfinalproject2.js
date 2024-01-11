82092bd50cad3d127669b2be3b15f8d2df9ee2bf
JohnnyZheng221 <127121193+JohnnyZheng221@users.noreply.github.com>
2024-01-12 04:16:10 +0800

    Create jsforfinalproject2.js

---
+++ jsforfinalproject2.js
@@ -0,0 +1,27 @@
+// 定義函數
+function showInfo() { 
+  // 獲取元素
+  var info = document.getElementById('ghg-info');
+  // 切換顯示或隱藏
+  info.style.display = info.style.display === 'none' ? 'block' : 'none';
+  // 改變顏色為紫色
+  document.getElementById('ghg').style.color = 'purple';
+}
+
+//Getting the button element
+var mybutton = document.getElementById("scrollBtn"); 
+//When user scrolls, scroll popup
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When user clicks button, scroll to top of page
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
