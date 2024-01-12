82ea2211cd132b61c4bfb0efd57189dd4a33039c
JohnnyZheng221 <127121193+JohnnyZheng221@users.noreply.github.com>
2024-01-12 15:48:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.getElementById("myImage").onclick = function() {
     this.classList.add('moving');
     document.getElementById('content').style.animation = 'fadeOut 0.3s';
     setTimeout(function() {
-        window.location.href = "FinalProject2.html";
+        window.location.href = "index2.html";
     }, 1000);
     this.classList.add('moved');  
