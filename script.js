a8f4bd1ae9ff9e2b9e5915848f575759e05e136c
JohnnyZheng221 <127121193+JohnnyZheng221@users.noreply.github.com>
2024-01-12 09:46:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,7 @@ document.getElementById("myImage").onclick = function() {
         window.location.href = "FinalProject2/FinalProject2.html";
     }, 1000);
     this.classList.add('moved');  
-
-var continents = ["Asia", "Africa", "North America", "South America", "Antarctica","Europe","Australia"];
-var weather = ["Drought", "Heat Waves", "Hurricanes, Typhoons and Cyclones", "Tornadoes", "Wildfires", "Winter Storms"];
-
-function createList(items) {
-    var list = document.createElement('ul');
-    for(var i = 0; i < items.length; i++) {
-        var item = document.createElement('li');
-        item.appendChild(document.createTextNode(items[i]));
-        list.appendChild(item);
-    }
-    return list;
-}
-
+    
 document.getElementById('ghg').addEventListener('click', function() {
     document.getElementById('ghg-info').style.display = 'block';
     this.classList.add('clicked');
